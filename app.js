9c44e0392a8e7e4cb8810e57b624b7ba936da14c
lawismahnem <lordlawrencesalazar@gmail.com>
2023-10-14 05:29:00 +0800

    new update with static and mongoDB

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
 const express = require ('express');
-
+const morgan = require ('morgan');
+const mongoose = require ('mongoose');
+const Blog = require ('./models/blog');
 // expres invoke function
+
+
 const app = express();
 
+//connection to mongodb
+const dbURI ='mongodb+srv://lawrence:lawrence321@cluster0.t2pet5h.mongodb.net/Cluster0?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err))
 app.set('view engine', 'ejs');
 
 //listen for rqeuest
 
-app.listen(3000);
+// midware & static files
+
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+app.get('/add-blog', (req,res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my blog lawrence',
+        body: 'more about my blog x lawrence'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+})
+
+app.get('/all-blog', (req,res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+
 app.get('/', (req,res)=>{
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},

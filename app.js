646f0500bf8bc8eed8836a4c955bb5feab7c48cd
lawismahnem <lordlawrencesalazar@gmail.com>
2023-10-14 02:48:39 +0800

    working on EJS instead of using HTML

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ const express = require ('express');
 // expres invoke function
 const app = express();
 
+app.set('view engine', 'ejs');
+
 //listen for rqeuest
 
 app.listen(3000);
 app.get('/', (req,res)=>{
     //res.send('<p>lawrence</p>');
-    res.sendFile('./views/index.html', { root:__dirname});
+    //res.sendFile('./views/index.html', { root:__dirname});
+    res.render('index');
 });
 
 app.get('/about', (req,res)=>{
-    res.sendFile('./views/about.html', { root:__dirname});
+    //res.sendFile('./views/about.html', { root:__dirname});
     //res.send('<p>lawrence about</p>');
+    res.render('about');
 });
 
+app.get('/blogs/create', (req,res) =>{
+   res.render('create');
+   
+});
 
-
+app.use((req,res) => {
+    res.status(404).sendFile('./views/404.html',{root:__dirname});
+    res.render('404');
+})
 //crash course tut #6

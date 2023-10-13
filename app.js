45db4272c713d1fb0bbf5c6923cf934ae84c7d9a
lawismahnem <lordlawrencesalazar@gmail.com>
2023-10-14 04:13:41 +0800

    update using EJS and modular way of handling html page

--- app.js
+++ app.js
@@ -9,24 +9,29 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 app.get('/', (req,res)=>{
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
     //res.send('<p>lawrence</p>');
     //res.sendFile('./views/index.html', { root:__dirname});
-    res.render('index');
+    res.render('index', { title: 'Lawrence', blogs});
 });
 
 app.get('/about', (req,res)=>{
     //res.sendFile('./views/about.html', { root:__dirname});
     //res.send('<p>lawrence about</p>');
-    res.render('about');
+    res.render('about', { title: 'about'});
 });
 
 app.get('/blogs/create', (req,res) =>{
-   res.render('create');
+   res.render('create', { title: 'create'});
    
 });
 
 app.use((req,res) => {
     res.status(404).sendFile('./views/404.html',{root:__dirname});
-    res.render('404');
+    res.render('404', { title: '404'});
 })
 //crash course tut #6
